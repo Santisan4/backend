8b09c969b541c25dafba22809258ffc22e1e8ed6
Santiago Sanchez <sant97@hotmail.com.ar>
2023-08-30 14:10:03 -0300

    new commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('node:path')
 
 const productRouter = require('./routes/product.js')
 const userRouter = require('./routes/user.js')
@@ -25,6 +26,18 @@ app.use(cors({
 app.use(express.json())
 app.use('/user', userRouter)
 app.use('/product', productRouter)
+
+// Serve static assets if in production
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+)
+
+// 404 not found
+app.use((req, res, next) => {
+  res.status(404).send('Sorry cant find that!')
+})
 
 const PORT = process.env.PORT ?? 3000
 
