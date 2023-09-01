2d96dd51da9012d1f9e3e9a72c981d4b13139cfc
Santiago Sanchez <sant97@hotmail.com.ar>
2023-09-01 15:32:40 -0300

    fixing errors

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 function isAuth (req, res, next) {
-  const authorization = req.get('authorization')
+  const authorization = req.get('Authorization')
   let token = ''
 
   if (authorization && authorization.toLowerCase().startsWith('bearer')) {
