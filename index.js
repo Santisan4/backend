fe9d6823804361aa77272c247bbaf2a2b76e9f68
Santiago Sanchez <sant97@hotmail.com.ar>
2023-09-03 16:42:14 -0300

    add static files

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ app.use('/admin', adminRouter)
 app.use('/user', userRouter)
 app.use('/products', productRouter)
 
-app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
 })
 
 // 404 not found
